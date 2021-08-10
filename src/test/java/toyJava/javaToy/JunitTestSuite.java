7769db866b694fece9b28fe3b5d50dc24b829d45
leusonmario <leusonmario@gmail.com>
2021-08-10 17:58:27 -0300

    new test suite

---
+++ src/test/java/toyJava/javaToy/JunitTestSuite.java
@@ -0,0 +1,14 @@
+package toyJava.javaToy;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    SomaTest.class,
+    SubTest.class
+})
+
+public class JunitTestSuite {
+}
